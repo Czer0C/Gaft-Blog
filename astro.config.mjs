defdcc1e4ee0c443f75a0184297732b5f2a886c0
TrieuNH7 <trieunh7@fpt.com.vn>
2024-09-03 00:33:55 +0700

    analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-sphere-demo.vercel.app",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 })
